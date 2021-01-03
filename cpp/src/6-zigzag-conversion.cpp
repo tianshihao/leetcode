2f34ec8da098e845095fea82d7d3fa54aef58556
tianshihao <tianshihao4944@126.com>
2021-01-03 15:37:53 +0800

    feat(cpp): solved problem 6

---
+++ cpp/src/6-zigzag-conversion.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    string convert(string s, int numRows)
+    {
+        // 如果只有 1 行.
+        if (numRows == 1)
+        {
+            return s;
+        }
+
+        vector<string> rows(min(numRows, int(s.size())));
+        int curRow = 0;
+        bool goingDown = false;
+
+        for (char c : s)
+        {
+            rows[curRow] += c;
+            if (curRow == 0 || curRow == numRows - 1)
+            {
+                goingDown = !goingDown;
+            }
+            curRow += goingDown ? 1 : -1;
+        }
+
+        // 将答案拼接起来.
+        string ret;
+        for (string row : rows)
+        {
+            ret += row;
+        }
+        return ret;
+    }
+};
+
+int main()
+{
+    Solution S;
+
+    cout << S.convert("LEETCODEISHIRING", 4) << endl;
+
+    return 0;
+}
