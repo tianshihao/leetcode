0ef66a0ab071d1accf076968c2ac52d4cfe0a9a8
tianshihao <tianshihao4944@126.com>
2021-01-03 13:35:30 +0800

    feat(cpp): solved problem 38

---
+++ cpp/src/38-count-and-say.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    string countAndSay(int n)
+    {
+        if (n == 1)
+        {
+            return "1";
+        }
+
+        string previous = countAndSay(n - 1);
+        string result = ""; // 使用递归来一层一层往前推
+        int count = 1;      // count用来计数
+
+        for (int i = 0; i < previous.length(); ++i)
+        {
+            if (previous[i] == previous[i + 1])
+            {
+                count++; // 比如previous是111221时，111部分会让count=3，此时i在第三个1处
+            }
+            else
+            {
+                result += to_string(count) + previous[i]; // result会从空变成“31”（当i在第三个1处时）
+                count = 1;                                // 由于i在第三个1处时，i+1处的值为2，1 != 2，所以count重新变成1
+            }
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    Solution S;
+    cout << S.countAndSay(4) << endl;
+
+    return 0;
+}
