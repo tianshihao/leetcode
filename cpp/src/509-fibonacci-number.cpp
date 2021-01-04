e0da69cb820c07a3537f252ff9bf2b5639edfa9c
tianshihao <tianshihao4944@126.com>
2021-01-04 12:36:37 +0800

    feat(cpp): solved problem 509

---
+++ cpp/src/509-fibonacci-number.cpp
@@ -0,0 +1,19 @@
+﻿class Solution
+{
+public:
+    int fib(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+        else if (n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return this->fib(n - 1) + this->fib(n - 2);
+        }
+    }
+};
