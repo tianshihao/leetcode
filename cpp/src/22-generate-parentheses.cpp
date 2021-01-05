69e5d01cdc78cbbb8ed96845c3298905d39227ff
tianshihao <tianshihao4944@126.com>
2021-01-05 14:03:00 +0800

    feat(cpp): solved problem 22

---
+++ cpp/src/22-generate-parentheses.cpp
@@ -0,0 +1,43 @@
+﻿#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+    void backtrack(vector<string> &ans, string &cur, int open, int close, int n)
+    {
+        if (cur.size() == n * 2)
+        {
+            ans.push_back(cur);
+            return;
+        }
+        // 左括号未满.
+        if (open < n)
+        {
+            cur.push_back('(');
+            backtrack(ans, cur, open + 1, close, n);
+            cur.pop_back();
+        }
+        // 右括号未满.
+        if (close < open)
+        {
+            cur.push_back(')');
+            backtrack(ans, cur, open, close + 1, n);
+            cur.pop_back();
+        }
+    }
+
+public:
+    vector<string> generateParenthesis(int n)
+    {
+        vector<string> result;
+        string current;
+        backtrack(result, current, 0, 0, n);
+        return result;
+    }
+};
+
+int main()
+{
+}
