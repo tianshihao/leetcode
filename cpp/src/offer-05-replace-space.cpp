c73eee548312f218aa271aca2d3f36d959e7bb66
tianshihao <tianshihao4944@126.com>
2021-01-14 19:12:25 +0800

    feat(cpp): solved offer problem 5

---
+++ cpp/src/offer-05-replace-space.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    string replaceSpace(string s)
+    {
+        string ans;
+
+        for (auto &c : s)
+        {
+            if (c == ' ')
+            {
+                ans.push_back('%');
+                ans.push_back('2');
+                ans.push_back('0');
+            }
+            else
+            {
+                ans.push_back(c);
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    return 0;
+}
