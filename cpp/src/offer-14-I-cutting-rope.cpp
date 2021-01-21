d0611d30c14ff3561714d6ab3cb972a094b90c3c
tianshihao <tianshihao4944@126.com>
2021-01-21 11:03:51 +0800

    feat: solved problem offer-14-I and 343

---
+++ cpp/src/offer-14-I-cutting-rope.cpp
@@ -0,0 +1,25 @@
+﻿class Solution
+{
+public:
+    int cuttingRope(int n)
+    {
+        if (n == 2)
+        {
+            return 1;
+        }
+        if (n == 3)
+        {
+            return 2;
+        }
+        vector<int> ans(n + 1);
+        ans[1] = 1;
+        ans[2] = 2;
+        ans[3] = 3;
+        for (int i = 4; i <= n; ++i)
+        {
+            ans[i] = max(2 * ans[i - 2], 3 * ans[i - 3]);
+        }
+
+        return ans[n];
+    }
+};
