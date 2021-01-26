571ea292ff36927e1a1a939fc23398810af80ce9
tianshihao <tianshihao4944@126.com>
2021-01-26 14:47:49 +0800

    solved problem 49

---
+++ src/49-group-anagrams.cpp
@@ -0,0 +1,28 @@
+﻿#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
+        unordered_map<string, vector<string>> m;
+        for (auto str : strs)
+        {
+            string key = str;
+            sort(key.begin(), key.end());
+            m[key].emplace_back(str);
+        }
+
+        vector<vector<string>> ans;
+        for (auto it = m.begin(); it != m.end(); ++it)
+        {
+            ans.emplace_back(it->second);
+        }
+
+        return ans;
+    }
+};
