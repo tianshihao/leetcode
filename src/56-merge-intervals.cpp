bbdfcf5b6dcdcff121816db6304e0fb570cf64be
tianshihao <tianshihao4944@126.com>
2021-01-26 15:54:12 +0800

    solved problem 56, 57

---
+++ src/56-merge-intervals.cpp
@@ -0,0 +1,50 @@
+﻿#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> merge(vector<vector<int>> &intervals)
+    {
+        if (intervals.size() == 0)
+        {
+            return {};
+        }
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> merged;
+        for (int i = 0; i < intervals.size(); ++i)
+        {
+            int L = intervals[i][0], R = intervals[i][1];
+            // 如果已合并的结果为空, 或者前一个区间和待放入区间不相交.
+            if (!merged.size() || merged.back()[1] < L)
+            {
+                merged.push_back({L, R});
+            }
+            // 已合并的最后一个区间的右边界小于待放入区间的左边界, 则区间重合.
+            // 而区间已经根据左边界排过序, 所以结果的最后一个元素的左边界
+            // 一定小于等于待放入区间左边界, 无需讨论, 只需找到这两个区间
+            // 那个有边界大, 更新右边界即可.
+            else
+            {
+                merged.back()[1] = max(merged.back()[1], R);
+            }
+        }
+        return merged;
+    }
+};
+
+int main()
+{
+    vector<vector<int>> intervals = {
+        {1, 3},
+        {2, 6},
+        {8, 10},
+        {15, 18}};
+
+    Solution S;
+
+    S.merge(intervals);
+
+    return 0;
+}
---
+++ src/57-insert-interval.cpp
@@ -0,0 +1,53 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> insert(vector<vector<int>> &intervals, vector<int> &newInterval)
+    {
+        // 插入区间左端点.
+        int left = newInterval[0];
+        // 插入区间右端点.
+        int right = newInterval[1];
+
+        vector<vector<int>> ans;
+        // 标记插入区间是否被插入.
+        bool flag = false;
+
+        // 考察每一个区间
+        for (auto &interval : intervals)
+        {
+            // 区间左端点大于插入区间的右端点, 说明没有交集. 无法合并, 将其加入答案.
+            if (interval[0] > right)
+            {
+                // 插入区间可以放置进来.
+                if (!flag)
+                {
+                    ans.push_back({left, right});
+                    flag = true;
+                }
+                ans.push_back(interval);
+            }
+            // 区间右端点小于插入区间的左端点, 说明也没有交集.
+            else if (interval[1] < left)
+            {
+                ans.push_back(interval);
+            }
+            // 否则就是有交集. 计算并集
+            else
+            {
+                // 相当于更新插入区间.
+                left = min(interval[0], left);
+                right = max(interval[1], right);
+            }
+        }
+        // 如果最后都没有被插入的话, 将插入区间插入.
+        if (!flag)
+        {
+            ans.push_back({left, right});
+        }
+
+        return ans;
+    }
+};
