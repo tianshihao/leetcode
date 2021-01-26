2fb1e39169c0b0cbd1403fc811d8621a8684be62
tianshihao <tianshihao4944@126.com>
2021-01-26 14:32:11 +0800

    solved problem 48

---
+++ src/46-permutations.cpp
@@ -0,0 +1,43 @@
+﻿#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    void backtrack(vector<vector<int>> &res, vector<int> &output, int first, int len)
+    {
+        // 所有的数都填完了.
+        if (first == len)
+        {
+            res.emplace_back(output);
+            return;
+        }
+        // 维护动态数组, output[first, len-1] 中为没有用过的数字, 循环从其中每次
+        // 拿出一个, 填到第 first 个位置, 即交换 output[i] 和 output[first].
+        // 交换完成即填写完毕, 回溯填写 first + 1 个位置. 然后撤销本次交换.
+        for (int i = first; i < len; ++i)
+        {
+            swap(output[i], output[first]);
+            backtrack(res, output, first + 1, len);
+            swap(output[i], output[first]);
+        }
+    }
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+        vector<vector<int>> res;
+        backtrack(res, nums, 0, (int)nums.size());
+        return res;
+    }
+};
+
+int main()
+{
+    vector<int> nums = {1, 2, 3};
+
+    Solution S;
+
+    S.permute(nums);
+
+    return 0;
+}
---
+++ src/48-rotate-image.cpp
@@ -0,0 +1,32 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size();
+        auto newMatrix = matrix;
+        vector<int> temp(n);
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = n - 1; j >= 0; --j)
+            {
+                newMatrix[j][n - 1 - i] = matrix[i][j];
+            }
+        }
+        matrix = newMatrix;
+    }
+};
+
+int main()
+{
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    Solution S;
+
+    S.rotate(matrix);
+
+    return 0;
+}
