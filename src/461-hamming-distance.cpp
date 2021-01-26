52c99e4cb373adea9611b192ab84b4279fa42a04
tianshihao <tianshihao4944@126.com>
2021-01-26 17:58:33 +0800

    feat: solved problem 461

---
+++ src/461-hamming-distance.cpp
@@ -0,0 +1,18 @@
+﻿#include <bitset>
+using namespace std;
+class Solution
+{
+public:
+    int hammingDistance(int x, int y)
+    {
+        return bitset<32>(x ^ y).count();
+    }
+};
+
+int main()
+{
+    Solution S;
+    S.hammingDistance(1, 4);
+
+    return 0;
+}
