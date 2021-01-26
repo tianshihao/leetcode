7eb8b82a170569308aa3949a7d971713515cc981
tianshihao <tianshihao4944@126.com>
2021-01-26 18:11:47 +0800

    feat: solved problem 206

---
+++ src/206-reverse-linked-list.cpp
@@ -0,0 +1,44 @@
+﻿struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        if (head == nullptr)
+        {
+            return nullptr;
+        }
+
+        ListNode *p, *r, *dummy;
+
+        // 用例没有头结点, 添加哑结点.
+        dummy = new ListNode(0, head);
+
+        p = dummy->next;
+
+        dummy->next = nullptr;
+
+        while (p != nullptr)
+        {
+            r = p->next;
+
+            p->next = dummy->next;
+            dummy->next = p;
+
+            p = r;
+        }
+
+        head = dummy->next;
+        delete dummy;
+
+        return head;
+    }
+};
