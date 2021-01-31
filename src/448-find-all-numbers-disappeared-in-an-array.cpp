414ec3914259b9638719447922c07637368a677b
tianshihao <tianshihao4944@126.com>
2021-01-31 11:39:42 +0800

    feat: solved problem 448

---
+++ src/448-find-all-numbers-disappeared-in-an-array.cpp
@@ -0,0 +1,53 @@
+﻿#include <cmath>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> findDisappearedNumbers(vector<int> &nums)
+    {
+        vector<int> ans;
+        int n = nums.size();
+
+        for (int i = 0; i < n; ++i)
+        {
+            // 将原数组中的元素取反, 作为 i + 1 出现过的标记.
+            if (nums[i] > 0)
+            {
+                if (nums[abs(nums[i] - 1)] > 0)
+                {
+                    nums[abs(nums[i] - 1)] *= -1;
+                }
+            }
+            // 不能重复标记.
+            else
+            {
+                if (nums[abs(-nums[i] - 1)] > 0)
+                {
+                    nums[abs(-nums[i] - 1)] *= -1;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; ++i)
+        {
+            if (nums[i] > 0)
+            {
+                ans.emplace_back(i + 1);
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    vector<int> nums = {1, 1};
+
+    Solution S;
+    S.findDisappearedNumbers(nums);
+
+    return 0;
+}
