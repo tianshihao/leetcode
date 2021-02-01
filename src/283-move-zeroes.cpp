e7bc5a1b6d5a83ca81a73b729840c8f1d981d9a9
tianshihao <tianshihao4944@126.com>
2021-02-01 19:12:56 +0800

    feat: solved problem 283

---
+++ src/283-move-zeroes.cpp
@@ -0,0 +1,26 @@
+﻿#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int n = nums.size(), left = 0, right = 0;
+        while (right < n)
+        {
+            if (nums[right])
+            {
+                swap(nums[left], nums[right]);
+                left++;
+            }
+            right++;
+        }
+    }
+};
+
+int main()
+{
+    return 0;
+}
