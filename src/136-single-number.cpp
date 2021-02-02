e98babedbf1f8912e456ec7e07a918079fc8f401
tianshihao <tianshihao4944@126.com>
2021-02-02 11:36:08 +0800

    feat: sovled problem 136

---
+++ src/136-single-number.cpp
@@ -0,0 +1,24 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int ret = 0;
+        for (auto e : nums)
+            ret ^= e;
+        return ret;
+    }
+};
+
+int main()
+{
+    vector<int> nums = {4, 1, 2, 1, 2};
+
+    Solution S;
+    S.singleNumber(nums);
+
+    return 0;
+}
