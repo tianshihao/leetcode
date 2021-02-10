77f50c64b6f0cf1bbff1bdaed9c8d349ff5992bf
tianshihao <tianshihao4944@126.com>
2021-02-10 21:07:27 +0800

    feat: solved problem 538

---
+++ src/538-convert-bst-to-greater-tree.cpp
@@ -0,0 +1,31 @@
+﻿struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+private:
+    int sum = 0;
+
+public:
+    /**
+     * @brief 反序中序遍历.
+     */
+    TreeNode *convertBST(TreeNode *root)
+    {
+        if (root != nullptr)
+        {
+            convertBST(root->right);
+            sum += root->val;
+            root->val = sum;
+            convertBST(root->left);
+        }
+        return root;
+    }
+};
