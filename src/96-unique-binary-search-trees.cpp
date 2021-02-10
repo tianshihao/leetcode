435f25f8dd42deb0a93888d7356cd39863486719
tianshihao <tianshihao4944@126.com>
2021-02-10 11:32:59 +0800

    feat: solved problem 96

---
+++ src/96-unique-binary-search-trees.cpp
@@ -0,0 +1,23 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int numTrees(int n)
+    {
+        vector<int> ans(n + 1, 0);
+        ans[0] = 1;
+        ans[1] = 1;
+
+        for (int i = 2; i <= n; ++i)
+        {
+            for (int j = 1; j <= i; ++j)
+            {
+                ans[i] += ans[j - 1] * ans[i - j];
+            }
+        }
+
+        return ans[n];
+    };
+};
