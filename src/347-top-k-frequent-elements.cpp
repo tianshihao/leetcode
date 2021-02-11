809d844bd9509018c9f06ffe4f4bc87e1a506e2a
tianshihao <tianshihao4944@126.com>
2021-02-11 15:19:05 +0800

    feat: solved problem 347

---
+++ src/347-top-k-frequent-elements.cpp
@@ -0,0 +1,53 @@
+﻿#include <queue>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    static bool cmp(pair<int, int> &m, pair<int, int> &n)
+    {
+        return m.second > n.second;
+    }
+
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> occurences;
+        for (auto &num : nums)
+        {
+            ++occurences[num];
+        }
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(&cmp)> q(cmp);
+
+        for (auto &[num, count] : occurences)
+        {
+            if (q.size() == k)
+            {
+                if (q.top().second < count)
+                {
+                    q.pop();
+                    q.emplace(num, count);
+                }
+            }
+            else
+            {
+                q.emplace(num, count);
+            }
+        }
+
+        vector<int> ans;
+        while (!q.empty())
+        {
+            ans.emplace_back(q.top().first);
+            q.pop();
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    return 0;
+}
