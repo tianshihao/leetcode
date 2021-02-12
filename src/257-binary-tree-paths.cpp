8b02ea90769cfbf2e210e0c5cad63bf8b348bbbf
tianshihao <tianshihao4944@126.com>
2021-02-12 09:57:16 +0800

    feat: solved problem 257

---
+++ src/257-binary-tree-paths.cpp
@@ -0,0 +1,51 @@
+﻿#include <cstring>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    void dfs(TreeNode *root, vector<string> &ans, string path)
+    {
+        if (root != nullptr)
+        {
+            path += to_string(root->val);
+
+            if (root->left == nullptr && root->right == nullptr)
+            {
+                ans.emplace_back(path);
+                return;
+            }
+            else
+            {
+                path += "->";
+                dfs(root->left, ans, path);
+                dfs(root->right, ans, path);
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    vector<string> binaryTreePaths(TreeNode *root)
+    {
+        vector<string> ans;
+
+        dfs(root, ans, "");
+
+        return ans;
+    }
+};
