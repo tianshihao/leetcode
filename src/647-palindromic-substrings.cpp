c60ea9ef33e730e2b629600e2ee2e9c97b7485ed
tianshihao <tianshihao4944@126.com>
2021-02-15 17:59:34 +0800

    feat: solved problem 647

---
+++ src/647-palindromic-substrings.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    int countSubstrings(string s)
+    {
+        int n = s.size(), ans = 0;
+        for (int i = 0; i < 2 * n - 1; ++i)
+        {
+            int left = i / 2;
+            int right = i / 2 + i % 2;
+            while (left >= 0 && right < n && s[left] == s[right])
+            {
+                --left;
+                ++right;
+                ++ans;
+            }
+        }
+
+        return ans;
+    }
+};
