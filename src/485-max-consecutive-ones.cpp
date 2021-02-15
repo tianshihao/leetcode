aea9a76fe87fef01df849088cab0ad79a6ba844a
tianshihao <tianshihao4944@126.com>
2021-02-15 09:49:08 +0800

    feat: solved problem 485

---
+++ src/485-max-consecutive-ones.cpp
@@ -0,0 +1,42 @@
+﻿#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int findMaxConsecutiveOnes(vector<int> &nums)
+    {
+        int ans = 0;
+        int count = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; ++i)
+        {
+            if (nums[i] == 1)
+            {
+                ++count;
+            }
+            else
+            {
+                ans = max(ans, count);
+                count = 0;
+            }
+        }
+
+        ans = max(ans, count);
+
+        return ans;
+    }
+};
+
+int main()
+{
+    vector<int> nums = {1};
+
+    Solution S;
+
+    S.findMaxConsecutiveOnes(nums);
+
+    return 0;
+}
