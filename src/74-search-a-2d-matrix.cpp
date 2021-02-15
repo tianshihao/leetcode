56dc8fcb745a463b415dd3b9208c580f3345b023
tianshihao <tianshihao4944@126.com>
2021-02-15 13:51:50 +0800

    feat: solved problem 74

---
+++ src/74-search-a-2d-matrix.cpp
@@ -0,0 +1,40 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        int rowNum = matrix.size();
+        if (rowNum == 0)
+        {
+            return 0;
+        }
+        int colNum = matrix[0].size();
+
+        int left = 0, right = rowNum * colNum - 1;
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            int midElem = matrix[mid / colNum][mid % colNum];
+            if (midElem == target)
+            {
+                return true;
+            }
+            else
+            {
+                if (target < midElem)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+        }
+        return false;
+    }
+};
