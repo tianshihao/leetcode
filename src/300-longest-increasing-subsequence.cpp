6ae4d6a59981a4d7c998daecefe8af85153893ee
tianshihao <tianshihao4944@126.com>
2021-02-15 16:03:45 +0800

    feat: solved problem 300

---
+++ src/300-longest-increasing-subsequence.cpp
@@ -0,0 +1,41 @@
+﻿#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n == 0)
+        {
+            return 0;
+        }
+        vector<int> dp(n, 0);
+
+        for (int i = 0; i < n; ++i)
+        {
+            dp[i] = 1;
+            for (int j = 0; j < i; ++j)
+            {
+                if (nums[j] < nums[i])
+                {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return *max_element(dp.begin(), dp.end());
+    }
+};
+
+int main()
+{
+    vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};
+
+    Solution S;
+    S.lengthOfLIS(nums);
+
+    return 0;
+}
