f2e5d27776c3eb400ea18b5e9ad9b3fffab32db3
tianshihao <tianshihao4944@126.com>
2021-02-16 17:28:44 +0800

    feat: solved problem 621

---
+++ src/621-task-scheduler.cpp
@@ -0,0 +1,30 @@
+﻿#include <algorithm>
+#include <numeric>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int leastInterval(vector<char> &tasks, int n)
+    {
+        unordered_map<char, int> freq;
+        for (char ch : tasks)
+        {
+            ++freq[ch];
+        }
+
+        int maxExec = max_element(freq.begin(), freq.end(),
+                                  [](const auto &u, const auto &v) -> bool {
+                                      return u.second < v.second;
+                                  })
+                          ->second;
+
+        int maxCount = accumulate(freq.begin(), freq.end(), 0, [](int acc, const auto &u) -> int {
+            return acc + (u.second == maxExec);
+        });
+
+        return max((maxExec - 1) * (n + 1) + maxCount, static_cast<int>(tasks.size()));
+    }
+};
