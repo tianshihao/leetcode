6dfbc6ca42dec89680115d24ac48926e0563f701
tianshihao <tianshihao4944@126.com>
2021-02-16 08:51:18 +0800

    feat: solved problem 561

---
+++ src/561-array-partition-i.cpp
@@ -0,0 +1,25 @@
+﻿#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int arrayPairSum(vector<int> &nums)
+    {
+        if (nums.empty())
+        {
+            return 0;
+        }
+
+        sort(nums.begin(), nums.end());
+
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i += 2)
+        {
+            ans += nums[i];
+        }
+
+        return ans;
+    }
+};
