06e4834074352a9f033fa64b35d759889aeaf8a8
tianshihao <tianshihao4944@126.com>
2021-02-19 14:31:15 +0800

    feat: solved problem 1004

---
+++ src/1004-max-consecutive-ones-iii.cpp
@@ -0,0 +1,29 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int longestOnes(vector<int> &A, int K)
+    {
+        int n = A.size();
+
+        int left = 0, right = 0;
+        int lsum = 0, rsum = 0;
+
+        int ans = 0;
+
+        for (right = 0; right < n; ++right)
+        {
+            rsum += 1 - A[right];
+            while (lsum < rsum - K)
+            {
+                lsum += 1 - A[left];
+                ++left;
+            }
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
